802cd1c46c35d59f977ca98e72a80fff02afa596
dodd <358683537@qq.com>
2021-08-23 17:33:15 +0800

    feat: remove cache method

--- src/Dispatcher/MASA.BuildingBlocks.Dispatcher.InMemory/IUnitOfWork.cs
+++ src/Dispatcher/MASA.BuildingBlocks.Dispatcher.InMemory/IUnitOfWork.cs
@@ -6,8 +6,6 @@ public interface IUnitOfWork : IDisposable
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 
-    Task<int> SaveChangesWithCacheAsync(CancellationToken cancellationToken = default);
-
     Task CommitAsync(CancellationToken cancellationToken = default);
 
     Task RollbackAsync(CancellationToken cancellationToken = default);
