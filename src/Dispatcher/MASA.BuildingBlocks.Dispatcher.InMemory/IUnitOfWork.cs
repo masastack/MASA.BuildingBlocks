d7d22f7737ae1c130a9760da14e8a898d57e04e1
dodd <358683537@qq.com>
2021-08-23 15:12:55 +0800

    feat: add IUnitOfWork

---
+++ src/Dispatcher/MASA.BuildingBlocks.Dispatcher.InMemory/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASA.BuildingBlocks.Dispatcher.InMemory
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        Task<int> SaveChangesWithCacheAsync(CancellationToken cancellationToken = default);
+
+        Task CommitAsync(CancellationToken cancellationToken = default);
+
+        Task RollbackAsync(CancellationToken cancellationToken = default);
+    }
+}
