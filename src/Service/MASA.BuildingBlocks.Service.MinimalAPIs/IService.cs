ac2f85a6e096f7cff86ca1c635c632e1e3414748
dodd <358683537@qq.com>
2021-09-08 17:09:14 +0800

    feat: add MASA.BuildingBlocks.Service.MinimalAPIs

---
+++ src/Service/MASA.BuildingBlocks.Service.MinimalAPIs/IService.cs
@@ -0,0 +1,9 @@
+﻿namespace MASA.BuildingBlocks.Service.MinimalAPIs;
+public interface IService
+{
+    IServiceCollection Services { get; }
+
+    TService? GetService<TService>();
+
+    TService GetRequiredService<TService>() where TService : notnull;
+}
