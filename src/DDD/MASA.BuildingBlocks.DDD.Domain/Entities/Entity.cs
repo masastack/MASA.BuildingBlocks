e4813f624cb415fd6c892f82d1f379597657689d
dodd <358683537@qq.com>
2021-09-09 12:54:46 +0800

    feat: add test

--- src/DDD/MASA.BuildingBlocks.DDD.Domain/Entities/Entity.cs
+++ src/DDD/MASA.BuildingBlocks.DDD.Domain/Entities/Entity.cs
@@ -12,6 +12,37 @@
 
             return $"{GetType().Name}:{connector}{string.Join(Environment.NewLine, keys.Select(key => $"{key.Name}={key.Value}"))}";
         }
+
+        public override bool Equals(object? obj)
+        {
+            var keys = GetKeys().ToArray();
+
+            if (this is null || obj is null) return false;
+
+            if (obj is Entity entity)
+            {
+                return entity.GetKeys().Select(key => key.Value).SequenceEqual(GetKeys().Select(key => key.Value));
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return GetKeys().Select(key => key.Value).Aggregate(0, (hashCode, next) => HashCode.Combine(hashCode, next));
+        }
+
+        public static bool operator ==(Entity x, Entity y)
+        {
+            return x.Equals(y);
+        }
+
+        public static bool operator !=(Entity x, Entity y)
+        {
+            return !x.Equals(y);
+        }
     }
 
     public class Entity<TKey> : Entity, IEntity<TKey>
---
+++ test/MASA.BuildingBlocks.DDD.Domain.Tests/TestEntity.cs
@@ -0,0 +1,48 @@
+namespace MASA.BuildingBlocks.DDD.Domain.Tests;
+[TestClass]
+public class TestEntity
+{
+    [TestMethod]
+    public void TestToString()
+    {
+        MasaEntity entity = new() { Id = Guid.Empty };
+        Assert.AreEqual("MasaEntity:Id=00000000-0000-0000-0000-000000000000", entity.ToString());
+    }
+
+    [TestMethod]
+    public void TestEquals()
+    {
+        var id = Guid.NewGuid();
+        MasaEntity x = new() { Id = id };
+        MasaEntity y = new() { Id = id };
+
+        Assert.IsTrue(x.Equals(y));
+    }
+
+    [TestMethod]
+    public void TestGetHashCode()
+    {
+        var id = Guid.NewGuid();
+        MasaEntity x = new() { Id = id };
+        MasaEntity y = new() { Id = id };
+
+        Assert.AreEqual(x.GetHashCode(), y.GetHashCode());
+    }
+
+    [TestMethod]
+    public void TestOperator()
+    {
+        var id = Guid.NewGuid();
+        MasaEntity x = new() { Id = id };
+        MasaEntity y = new() { Id = id };
+        MasaEntity z = new() { Id = Guid.NewGuid() };
+
+        Assert.IsTrue(x == y);
+        Assert.IsTrue(x != z);
+    }
+
+    public class MasaEntity : Entity<Guid>
+    {
+
+    }
+}
