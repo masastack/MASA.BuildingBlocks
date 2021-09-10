3431baff176a011c6b13619d5b2a64f339a01299
dodd <358683537@qq.com>
2021-09-10 12:14:20 +0800

    feat: add IMiddleware

---
+++ src/Dispatcher/MASA.BuildingBlocks.Dispatcher.InMemory/IMiddleware.cs
@@ -0,0 +1,12 @@
+﻿namespace MASA.BuildingBlocks.Dispatcher.InMemory;
+
+public delegate Task EventHandlerDelegate();
+
+/// <summary>
+/// Middleware is assembled into an event pipeline to handle invoke event and result
+/// </summary>
+public interface IMiddleware<TEvent, TResult>
+    where TEvent : notnull
+{
+    Task<TResult> HandleAsync(TEvent @event, EventHandlerDelegate next);
+}
