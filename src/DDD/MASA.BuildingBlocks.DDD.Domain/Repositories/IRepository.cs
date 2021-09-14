5d58cdbf6b84d94538feb929e73edbc3b8ec8bf2
dodd <358683537@qq.com>
2021-09-14 16:53:41 +0800

    feat: add IRepository

---
+++ src/DDD/MASA.BuildingBlocks.DDD.Domain/Repositories/IRepository.cs
@@ -0,0 +1,52 @@
+﻿namespace MASA.BuildingBlocks.DDD.Domain.Repositories;
+public interface IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    #region Add
+
+    ValueTask<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+    Task AddRangeAsync(params TEntity[] entities);
+
+    Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+    #endregion
+
+    #region Update
+
+    ValueTask<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+    Task UpdateRangeAsync(params TEntity[] entities);
+
+    Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+    #endregion
+
+    #region Remove
+
+    ValueTask<TEntity> RemoveAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+    Task RemoveRangeAsync(params TEntity[] entities);
+
+    Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+    Task RemoveAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+
+    #endregion
+
+    #region Find
+
+    ValueTask<TEntity?> FindAsync(params object?[]? keyValues);
+
+    ValueTask<TEntity?> FindAsync(object?[]? keyValues, CancellationToken cancellationToken);
+
+    Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+
+    #endregion
+}
+
+public interface IRepository<TEntity, TKey> : IRepository<TEntity>
+    where TEntity : class, IEntity<TKey>
+{
+
+}
--- src/DDD/MASA.BuildingBlocks.DDD.Domain/_Imports.cs
+++ src/DDD/MASA.BuildingBlocks.DDD.Domain/_Imports.cs
@@ -1,5 +1,7 @@
 ﻿global using MASA.BuildingBlocks.Data.Contracts;
+global using MASA.BuildingBlocks.DDD.Domain.Entities;
 global using MASA.BuildingBlocks.Dispatcher.InMemory;
 global using MASA.BuildingBlocks.Dispatcher.Remoting;
 global using MASA.BuildingBlocks.ReadWriteSpliting.CQRS.Commands;
 global using MASA.BuildingBlocks.ReadWriteSpliting.CQRS.Queries;
+global using System.Linq.Expressions;
