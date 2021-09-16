b324f27128174517eca932d83a37d8c57b138cd3
dodd <358683537@qq.com>
2021-09-16 09:16:13 +0800

    feat: add IDomainService

--- src/DDD/MASA.BuildingBlocks.DDD.Domain/_Imports.cs
+++ src/DDD/MASA.BuildingBlocks.DDD.Domain/_Imports.cs
@@ -1,6 +1,7 @@
 ﻿global using MASA.BuildingBlocks.Data.Contracts;
 global using MASA.BuildingBlocks.Data.Uow;
 global using MASA.BuildingBlocks.DDD.Domain.Entities;
+global using MASA.BuildingBlocks.DDD.Domain.Events;
 global using MASA.BuildingBlocks.Dispatcher.InMemory;
 global using MASA.BuildingBlocks.Dispatcher.Remoting;
 global using MASA.BuildingBlocks.ReadWriteSpliting.CQRS.Commands;
