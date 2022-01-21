f364d1c555ec06728efa5140aa51750bc8e1b806
zhenlei520 <wangzhenlei520@gmail.com>
2022-01-21 14:43:43 +0800

    chore: Add GetCurrentTime method

--- src/DDD/MASA.BuildingBlocks.DDD.Domain/Entities/Auditing/AuditEntity.cs
+++ src/DDD/MASA.BuildingBlocks.DDD.Domain/Entities/Auditing/AuditEntity.cs
@@ -1,15 +1,26 @@
-﻿namespace MASA.BuildingBlocks.DDD.Domain.Entities.Auditing;
+namespace MASA.BuildingBlocks.DDD.Domain.Entities.Auditing;
+
 public abstract class AuditEntity<TUserId> : Entity, IAuditEntity<TUserId>
 {
     public bool IsDeleted { get; set; }
 
     public TUserId Creator { get; set; } = default!;
 
-    public DateTime CreationTime { get; set; } = DateTime.UtcNow;
+    public DateTime CreationTime { get; set; }
 
     public TUserId Modifier { get; set; } = default!;
 
-    public DateTime ModificationTime { get; set; } = DateTime.UtcNow;
+    public DateTime ModificationTime { get; set; }
+
+    public AuditEntity() => Initialize();
+
+    public void Initialize()
+    {
+        this.CreationTime = this.GetCurrentTime();
+        this.ModificationTime = this.GetCurrentTime();
+    }
+
+    public virtual DateTime GetCurrentTime() => DateTime.UtcNow;
 }
 
 public class AuditEntity<TKey, TUserId> : Entity<TKey>, IAuditEntity<TUserId>
@@ -23,4 +34,14 @@ public class AuditEntity<TKey, TUserId> : Entity<TKey>, IAuditEntity<TUserId>
     public TUserId Modifier { get; set; } = default!;
 
     public DateTime ModificationTime { get; set; } = DateTime.UtcNow;
+
+    public AuditEntity() => Initialize();
+
+    public void Initialize()
+    {
+        this.CreationTime = this.GetCurrentTime();
+        this.ModificationTime = this.GetCurrentTime();
+    }
+
+    public virtual DateTime GetCurrentTime() => DateTime.UtcNow;
 }
