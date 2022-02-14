7a3064ef92b9d467931e6d5fe245c55e07ac4aa3
zhenlei520 <wangzhenlei@gmail.com>
2022-02-14 17:09:44 +0800

    chore: add entity state

--- src/Data/MASA.BuildingBlocks.Data.UoW/IUnitOfWork.cs
+++ src/Data/MASA.BuildingBlocks.Data.UoW/IUnitOfWork.cs
@@ -1,13 +1,24 @@
 namespace MASA.BuildingBlocks.Data.UoW;
-public interface IUnitOfWork : IAsyncDisposable
+public interface IUnitOfWork : IDisposable, IAsyncDisposable
 {
     DbTransaction Transaction { get; }
 
+    /// <summary>
+    /// Whether the transaction has been opened
+    /// </summary>
     bool TransactionHasBegun { get; }
 
+    /// <summary>
+    /// Do you need to use transactions
+    /// </summary>
     bool UseTransaction { get; set; }
 
+    /// <summary>
+    /// Disable transaction rollback after failure
+    /// </summary>
     bool DisableRollbackOnFailure { get; set; }
+
+    EntityState EntityState { get; set; }
 
     Task SaveChangesAsync(CancellationToken cancellationToken = default);
 
