e7e86d4c88b3ddeaaefb798c2388f2b8b5514282
zhenlei520 <wangzhenlei520@gmail.com>
2022-03-31 11:29:00 +0800

    chore: Add CreateDbContext annotation

--- src/Data/Masa.BuildingBlocks.Data.UoW/IUnitOfWorkManager.cs
+++ src/Data/Masa.BuildingBlocks.Data.UoW/IUnitOfWorkManager.cs
@@ -3,7 +3,8 @@
 public interface IUnitOfWorkManager
 {
     /// <summary>
-    ///
+    /// Create new DbContext
+    /// We create DbContext with lazy loading enabled by default
     /// </summary>
     /// <param name="lazyLoading">Deferred creation of DbContext, easy to specify tenant or environment by yourself, which is very effective for physical isolation</param>
     /// <returns></returns>
