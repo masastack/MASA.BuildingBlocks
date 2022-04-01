31a1470ebeb7834f134570def5aee5c0888a538c
zhenlei520 <wangzhenlei520@gmail.com>
2022-04-01 14:44:16 +0800

    refactor(Isolation): refactor ParserProvider

---
+++ src/Isolation/Masa.BuildingBlocks.Isolation/IParserProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Masa.BuildingBlocks.Isolation;
+
+public interface IParserProvider
+{
+    string Name { get; }
+
+    Task<bool> ResolveAsync(IServiceProvider serviceProvider, string key, Action<string> action);
+}
