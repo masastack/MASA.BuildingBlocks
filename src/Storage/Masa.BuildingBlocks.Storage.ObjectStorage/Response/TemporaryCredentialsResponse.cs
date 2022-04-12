8b56ced5cd2ec42fab101fccb36d09a9e584b460
zhenlei520 <wangzhenlei520@gmail.com>
2022-04-12 15:28:11 +0800

    feature(Storage): add cloud storage

---
+++ src/Storage/Masa.BuildingBlocks.Storage.ObjectStorage/Response/TemporaryCredentialsResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Masa.BuildingBlocks.Storage.ObjectStorage.Response;
+public class TemporaryCredentialsResponse
+{
+    public string AccessKeyId { get; }
+
+    public string SecretAccessKey { get; }
+
+    public string SessionToken { get; }
+
+    public string Expiration { get; }
+
+    public TemporaryCredentialsResponse(string accessKeyId, string secretAccessKey, string sessionToken, string expiration)
+    {
+        AccessKeyId = accessKeyId;
+        SecretAccessKey = secretAccessKey;
+        SessionToken = sessionToken;
+        Expiration = expiration;
+    }
+}
