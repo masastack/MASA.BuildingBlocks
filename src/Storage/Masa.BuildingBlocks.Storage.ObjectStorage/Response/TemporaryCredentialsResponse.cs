489953e041412568b4ec8fae1eac58eed585fc2c
听雨声 <wangzhenlei520@gmail.com>
2022-04-12 16:20:21 +0800

    feature(Storage): add cloud storage (#33)
    * feature(Storage): add cloud storage
    
    * chore: change SecretAccessKey to AccessKeySecret
    
    * chore: Remove ResponseBase

---
+++ src/Storage/Masa.BuildingBlocks.Storage.ObjectStorage/Response/TemporaryCredentialsResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Masa.BuildingBlocks.Storage.ObjectStorage.Response;
+public class TemporaryCredentialsResponse
+{
+    public string AccessKeyId { get; }
+
+    public string AccessKeySecret { get; }
+
+    public string SessionToken { get; }
+
+    public DateTime? Expiration { get; }
+
+    public TemporaryCredentialsResponse(string accessKeyId, string accessKeySecret, string sessionToken, DateTime? expiration)
+    {
+        AccessKeyId = accessKeyId;
+        AccessKeySecret = accessKeySecret;
+        SessionToken = sessionToken;
+        Expiration = expiration;
+    }
+}
---
+++ src/Storage/Masa.BuildingBlocks.Storage.ObjectStorage/_Imports.cs
@@ -0,0 +1,1 @@
+﻿global using Masa.BuildingBlocks.Storage.ObjectStorage.Response;
