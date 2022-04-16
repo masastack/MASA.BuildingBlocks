59279247c5abef985026ae63495ebb36b7348245
xAmast <58246546+xAmast@users.noreply.github.com>
2022-04-16 15:44:34 +0800

    feat(Domain): Add reservation of AggregateRoot<T> to domain events. (#37)
    * feat(Domain): Add reservation of AggregateRoot to domain events.
    
    * feat(Domain): Add reservation of AggregateRoot<T> to domain events.

--- src/Ddd/Masa.BuildingBlocks.Ddd.Domain/Entities/AggregateRoot.cs
+++ src/Ddd/Masa.BuildingBlocks.Ddd.Domain/Entities/AggregateRoot.cs
@@ -26,5 +26,25 @@ public abstract class AggregateRoot : Entity, IAggregateRoot
 
 public class AggregateRoot<TKey> : Entity<TKey>, IAggregateRoot<TKey>
 {
+    protected List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
 
+    public virtual void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public virtual void RemoveDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Remove(domainEvent);
+    }
+
+    public virtual IEnumerable<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents;
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
 }
