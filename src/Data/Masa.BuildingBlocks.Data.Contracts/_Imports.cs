de29342a07b16be885569f9d2eeb35476086d7bb
zhenlei520 <wangzhenlei520@gmail.com>
2022-04-29 17:43:57 +0800

    feat(Data): Add ISoftDelete

---
+++ src/Data/Masa.BuildingBlocks.Data.Contracts/_Imports.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+global using System.Collections.Concurrent;
+global using Masa.BuildingBlocks.Data.Contracts.Internal;
+global using Microsoft.Extensions.DependencyInjection;
