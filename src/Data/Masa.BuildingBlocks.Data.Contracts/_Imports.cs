80235ccccdaa2ab5cfcfb29ae8c0e0e15c9e08ca
zhenlei520 <wangzhenlei520@gmail.com>
2022-05-09 09:31:46 +0800

    chore: Global Using Sort

--- src/Data/Masa.BuildingBlocks.Data.Contracts/_Imports.cs
+++ src/Data/Masa.BuildingBlocks.Data.Contracts/_Imports.cs
@@ -1,6 +1,5 @@
-﻿// Copyright (c) MASA Stack All rights reserved.
+// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-global using System.Collections.Concurrent;
-global using Masa.BuildingBlocks.Data.Contracts.Internal;
 global using Microsoft.Extensions.DependencyInjection;
+global using System.Collections.Concurrent;
--- src/Data/Masa.BuildingBlocks.Data/_Imports.cs
+++ src/Data/Masa.BuildingBlocks.Data/_Imports.cs
@@ -2,5 +2,4 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.Data.Options;
-global using System.Data.Common;
 global using System.Reflection;
