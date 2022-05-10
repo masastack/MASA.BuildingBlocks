55c5d96d14ec5092a298a07453dadf67ba32fec7
MayueCif <6133287+MayueCif@users.noreply.github.com>
2022-05-10 12:40:48 +0800

    feat(service):add service contracts (#50)
    * feat(service):add service contracts
    
    * chore:modify comment
    
    Co-authored-by: Mayue <mayue@lonsid.cn>

---
+++ src/Service/Masa.BuildingBlocks.Service.Contracts/BaseResponse.cs
@@ -0,0 +1,16 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.Service.Contracts;
+
+public abstract class BaseResponse : BaseMessage
+{
+    public BaseResponse(Guid correlationId) : base()
+    {
+        base._correlationId = correlationId;
+    }
+
+    public BaseResponse()
+    {
+    }
+}
