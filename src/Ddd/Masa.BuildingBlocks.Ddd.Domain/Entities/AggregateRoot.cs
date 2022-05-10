8f97528e15f3fb2c6975c9c0a1a03264631bf2ae
zhenlei520 <wangzhenlei520@gmail.com>
2022-05-10 17:05:38 +0800

    feat(DDD): Add IGeneratesDomainEvents

--- src/Ddd/Masa.BuildingBlocks.Ddd.Domain/Entities/AggregateRoot.cs
+++ src/Ddd/Masa.BuildingBlocks.Ddd.Domain/Entities/AggregateRoot.cs
@@ -3,7 +3,7 @@
 
 namespace Masa.BuildingBlocks.Ddd.Domain.Entities;
 
-public abstract class AggregateRoot : Entity, IAggregateRoot
+public abstract class AggregateRoot : Entity, IGeneratesDomainEvents
 {
     protected List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
 
@@ -37,7 +37,7 @@ public abstract class AggregateRoot : Entity, IAggregateRoot
     }
 }
 
-public class AggregateRoot<TKey> : Entity<TKey>, IAggregateRoot<TKey>
+public class AggregateRoot<TKey> : Entity<TKey>, IAggregateRoot<TKey>, IGeneratesDomainEvents
 {
     protected List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
 
