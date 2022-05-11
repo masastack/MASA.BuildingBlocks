fcd4c1f099a80dab68baad6fec343acdbbf97d95
zhenlei520 <wangzhenlei520@gmail.com>
2022-05-11 23:46:37 +0800

    feat(Storage): Add GetObject、PutObject、ObjectExists、DeleteObject method

--- src/Storage/Masa.BuildingBlocks.Storage.ObjectStorage/IClient.cs
+++ src/Storage/Masa.BuildingBlocks.Storage.ObjectStorage/IClient.cs
@@ -17,32 +17,32 @@ public interface IClient
     /// <returns></returns>
     string GetToken();
 
-    // Task GetObjectAsync(
-    //     string bucketName,
-    //     string objectName,
-    //     Action<Stream> callback,
-    //     CancellationToken cancellationToken = default(CancellationToken));
+    Task GetObjectAsync(
+        string bucketName,
+        string objectName,
+        Action<Stream> callback,
+        CancellationToken cancellationToken = default(CancellationToken));
 
-    //Task GetObjectAsync(
-    //    string bucketName,
-    //    string objectName,
-    //    long offset,
-    //    long length,
-    //    Action<Stream> callback,
-    //    CancellationToken cancellationToken = default(CancellationToken));
+    Task GetObjectAsync(
+        string bucketName,
+        string objectName,
+        long offset,
+        long length,
+        Action<Stream> callback,
+        CancellationToken cancellationToken = default(CancellationToken));
 
-    // Task PutObjectAsync(
-    //     string bucketName,
-    //     string objectName,
-    //     Stream data,
-    //     CancellationToken cancellationToken = default(CancellationToken));
+    Task PutObjectAsync(
+        string bucketName,
+        string objectName,
+        Stream data,
+        CancellationToken cancellationToken = default(CancellationToken));
 
-    //Task<bool> ObjectExistsAsync(
-    //    string bucketName,
-    //    string objectName,
-    //    CancellationToken cancellationToken = default(CancellationToken));
+    Task<bool> ObjectExistsAsync(
+        string bucketName,
+        string objectName,
+        CancellationToken cancellationToken = default(CancellationToken));
 
-    // Task DeleteObjectAsync(string bucketName, string objectName, CancellationToken cancellationToken = default(CancellationToken));
+    Task DeleteObjectAsync(string bucketName, string objectName, CancellationToken cancellationToken = default(CancellationToken));
 
-    // Task DeleteObjectAsync(string bucketName, IEnumerable<string> objectNames, CancellationToken cancellationToken = default(CancellationToken));
+    Task DeleteObjectAsync(string bucketName, IEnumerable<string> objectNames, CancellationToken cancellationToken = default(CancellationToken));
 }
