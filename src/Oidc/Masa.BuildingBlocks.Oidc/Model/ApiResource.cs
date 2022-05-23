9b22f25ff8ebb3eb12835af6cbc6590a7ad580cc
wuweilai <824255785@qq.com>
2022-05-23 09:49:16 +0800

    Code optimization

--- src/Oidc/Masa.BuildingBlocks.Oidc/Model/ApiResource.cs
+++ src/Oidc/Masa.BuildingBlocks.Oidc/Model/ApiResource.cs
@@ -2,10 +2,6 @@ namespace Masa.BuildingBlocks.Oidc.Model;
 
 public class ApiResource : Resource
 {
-    public bool Required { get; set; }
-
-    public bool Emphasize { get; set; }
-
     public ICollection<string>? Scopes { get; set; }
 
     public ICollection<Secret>? ApiSecrets { get; set; }
@@ -19,13 +15,11 @@ public class ApiResource : Resource
         if(userClaims is not null) UserClaims = userClaims;
     }
 
-    public ApiResource(string name, string displayName, string? description, bool enabled, bool showInDiscoveryDocument, bool required, bool emphasize, ICollection<string>? userClaims, ICollection<string>? scopes, IDictionary<string, string>? properties, ICollection<Secret>? apiSecrets, ICollection<string>? allowedAccessTokenSigningAlgorithms) : this(name, displayName, userClaims)
+    public ApiResource(string name, string displayName, string? description, bool enabled, bool showInDiscoveryDocument, ICollection<string>? userClaims, ICollection<string>? scopes, IDictionary<string, string>? properties, ICollection<Secret>? apiSecrets, ICollection<string>? allowedAccessTokenSigningAlgorithms) : this(name, displayName, userClaims)
     {
         Description = description;
         Enabled = enabled;
         ShowInDiscoveryDocument = showInDiscoveryDocument;
-        Required = required;
-        Emphasize = emphasize;
         Scopes = scopes;
         Properties = properties;
         ApiSecrets = apiSecrets;
