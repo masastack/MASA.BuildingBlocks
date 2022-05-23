939c607bce2b2591bbefb246631767647508792f
wuweilai <824255785@qq.com>
2022-05-23 17:56:19 +0800

    auth sdk

---
+++ src/BasicAbility/Masa.BuildingBlocks.BasicAbility.Auth/Model/AddUserModel.cs
@@ -0,0 +1,30 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.BasicAbility.Auth.Model;
+
+public class AddUserModel
+{
+    public string Account { get; set; }
+
+    public string Name { get; set; }
+
+    public string? DisplayName { get; set; }
+
+    public string? IdCard { get; set; }
+ 
+    public string? CompanyName { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Email { get; set; }
+
+    public GenderTypes Gender { get; set; }
+
+    public AddUserModel(string account, string name)
+    {
+        Account = account;
+        Name = name;
+    }
+}
+
