d346b3934033ce4003108af117c2f8634b18576e
磊磊 <wangzhenlei520@gmail.com>
2022-06-14 12:43:11 +0800

    feat: Add IUserSetter and IUserContext (#63)
    * auth sdk
    
    * optimize code
    
    * feat(Parser): Modify the parser and Identity
    
    * feat: Add Masa.BuildingBlocks.Identity
    
    * chore: Adjust the parser
    
    * chore: remove extra spaces
    
    * refactor(Identity): Refactor IdentityModel
    
    * chore: change implementation
    
    * refactor(Identity): Refactor Identity
    
    * chore:  Modify class name
    
    * chore: Add class library
    
    Co-authored-by: wuweilai <824255785@qq.com>

---
+++ src/Identity/Masa.BuildingBlocks.IdentityModel/IUserContext.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.IdentityModel;
+
+public interface IUserContext
+{
+    bool IsAuthenticated { get; }
+
+    string? UserId { get; }
+
+    string? UserName { get; }
+
+    TUserId? GetUserId<TUserId>();
+
+    TIdentityUser? GetUser<TIdentityUser>() where TIdentityUser : IIdentityUser;
+}
