8a5a817767d52b2f8d51b6cdbf5c652abe3bdfd6
zhenlei520 <wangzhenlei520@gmail.com>
2022-06-17 16:11:10 +0800

    feat(Storage): Add IClientContainer, reduce the input bucketName

---
+++ src/Storage/Masa.BuildingBlocks.Storage.ObjectStorage/BucketNameAttribute.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.Storage.ObjectStorage;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class BucketNameAttribute : Attribute
+{
+    public string? BucketName { get; set; }
+
+    public BucketNameAttribute(string? bucketName = null) => BucketName = bucketName;
+
+    public static string GetBucketName<T>() => GetBucketName(typeof(T));
+
+    public static string GetBucketName(Type type)
+    {
+        var nameAttribute = type.GetTypeInfo().GetCustomAttribute<BucketNameAttribute>();
+
+        if (nameAttribute == null)
+            throw new Exception("");
+
+        return !string.IsNullOrWhiteSpace(nameAttribute.BucketName) ? nameAttribute.BucketName : type.Name;
+    }
+}
