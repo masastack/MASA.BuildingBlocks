8ed77e98eb7ae19609f4a2532a2241a644f6848e
wuweilai <30889371+15168440402@users.noreply.github.com>
2022-06-20 17:11:39 +0800

    feature(oidc):add oidc (#66)
    * add IClientStore、IResourceStore
    
    * Code optimization
    
    * feat(oidc):oidc
    
    * feat(oidc):oidc
    
    * feat(oidc):oidc
    
    * feat(oidc):oidc
    
    * feat(oidc):oidc
    
    * feat(oidc):oidc
    
    * feat:update client
    
    * feat:update cache
    
    * feat(oidc):oidc
    
    * feat(oidc):oidc
    
    * feat(oidc):oidc
    
    * refactor:formatting code
    
    * feat(oidc):oidc
    
    * feat(oidc):remove AddAllAsync,add ResetAsync
    
    * feat(oidc):sln
    
    * refactor:refactor code
    
    * refactor(code):refactor code
    
    * refactor:refactor code
    
    * refactor:refactor code
    
    * fix:ReUse change to Reuse
    
    * refactor(code):refactor code
    
    Co-authored-by: Mayue <mayue@lonsid.cn>

---
+++ src/Authentication/Masa.BuildingBlocks.Authentication.Oidc.Models/Constans/GrantTypes.cs
@@ -0,0 +1,37 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.Authentication.Oidc.Models.Constans;
+
+public class GrantTypes
+{
+    public static ICollection<string> Implicit =>
+        new[] { GrantType.Implicit };
+
+    public static ICollection<string> ImplicitAndClientCredentials =>
+        new[] { GrantType.Implicit, GrantType.ClientCredentials };
+
+    public static ICollection<string> Code =>
+        new[] { GrantType.AuthorizationCode };
+
+    public static ICollection<string> CodeAndClientCredentials =>
+        new[] { GrantType.AuthorizationCode, GrantType.ClientCredentials };
+
+    public static ICollection<string> Hybrid =>
+        new[] { GrantType.Hybrid };
+
+    public static ICollection<string> HybridAndClientCredentials =>
+        new[] { GrantType.Hybrid, GrantType.ClientCredentials };
+
+    public static ICollection<string> ClientCredentials =>
+        new[] { GrantType.ClientCredentials };
+
+    public static ICollection<string> ResourceOwnerPassword =>
+        new[] { GrantType.ResourceOwnerPassword };
+
+    public static ICollection<string> ResourceOwnerPasswordAndClientCredentials =>
+        new[] { GrantType.ResourceOwnerPassword, GrantType.ClientCredentials };
+
+    public static ICollection<string> DeviceFlow =>
+        new[] { GrantType.DeviceFlow };
+}
