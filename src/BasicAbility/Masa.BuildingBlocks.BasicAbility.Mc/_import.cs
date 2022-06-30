1dc37ce69dba7e1661de6c2d0e31fc00593d217a
吴自浩 <wuzihao425@outlook.com>
2022-06-30 10:32:14 +0800

    refactor：process code review

--- src/BasicAbility/Masa.BuildingBlocks.BasicAbility.Mc/_import.cs
+++ src/BasicAbility/Masa.BuildingBlocks.BasicAbility.Mc/_import.cs
@@ -2,13 +2,7 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.BasicAbility.Mc.Enum;
-global using Masa.BuildingBlocks.BasicAbility.Mc.Infrastructure.Helper;
 global using Masa.BuildingBlocks.BasicAbility.Mc.Model;
 global using Masa.BuildingBlocks.BasicAbility.Mc.Service;
-global using Masa.BuildingBlocks.Ddd.Domain.Entities.Auditing;
 global using Masa.BuildingBlocks.Ddd.Domain.Repositories;
 global using System.Collections.Concurrent;
-global using System.ComponentModel;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Globalization;
-global using System.Reflection;
