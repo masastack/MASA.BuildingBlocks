8db972882b0ed27a07a0a6159ae7f060643eee2c
Mayue <mayue@lonsid.cn>
2022-07-04 09:13:06 +0800

    feat:add user role

--- src/Identity/Masa.BuildingBlocks.Identity.IdentityModel/Entities/IdentityUser.cs
+++ src/Identity/Masa.BuildingBlocks.Identity.IdentityModel/Entities/IdentityUser.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) MASA Stack All rights reserved.
+// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 namespace Masa.BuildingBlocks.Identity.IdentityModel;
@@ -8,4 +8,6 @@ public class IdentityUser : IIdentityUser
     public string Id { get; set; }
 
     public string? UserName { get; set; }
+
+    public IEnumerable<IdentityRole<string>> Roles { get; set; } = new List<IdentityRole<string>>();
 }
