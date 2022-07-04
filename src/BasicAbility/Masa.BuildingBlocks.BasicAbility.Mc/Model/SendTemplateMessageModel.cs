49374b7386e7d73ed4d6f8248824bfc18c2fd31d
wzh425 <wuzihao425@outlook.com>
2022-07-04 09:34:15 +0800

    feat：mc sdk & sdk unit test (#83)
    * feat(websiteMessage)：mc sdk
    
    * feat: mc sdk & sdk unit test
    
    * refactor：add model base class
    
    * refactor：process code review
    
    * refactor：process code review
    
    * refactor：rename
    
    * refactor：_import.cs => _Imports.cs
    
    * refactor：rename
    
    * refactor：rename
    
    * refactor：rename

---
+++ src/BasicAbility/Masa.BuildingBlocks.BasicAbility.Mc/Model/SendTemplateMessageModel.cs
@@ -0,0 +1,42 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.BasicAbility.Mc.Model;
+
+public class SendTemplateMessageModel
+{
+    public Guid ChannelId { get; set; }
+
+    public ChannelTypes? ChannelType { get; set; }
+
+    public Guid EntityId { get; set; }
+
+    public SendTargets ReceiverType { get; set; }
+
+    public string Sign { get; set; } = string.Empty;
+
+    public List<MessageTaskReceiverModel> Receivers { get; set; } = new();
+
+    public SendRuleModel SendRules { get; set; } = new();
+
+    public ExtraPropertyDictionary Variables { get; set; } = new();
+
+    public static implicit operator MessageTaskUpsertModel(SendTemplateMessageModel model)
+    {
+        return new MessageTaskUpsertModel
+        {
+            ChannelId = model.ChannelId,
+            ChannelType = model.ChannelType,
+            EntityType = MessageTypes.Template,
+            EntityId = model.EntityId,
+            IsDraft = false,
+            IsEnabled = true,
+            ReceiverType = model.ReceiverType,
+            SelectReceiverType = MessageTaskSelectReceiverTypes.ManualSelection,
+            Sign = model.Sign,
+            Receivers = model.Receivers,
+            SendRules = model.SendRules,
+            Variables = model.Variables,
+        };
+    }
+}
