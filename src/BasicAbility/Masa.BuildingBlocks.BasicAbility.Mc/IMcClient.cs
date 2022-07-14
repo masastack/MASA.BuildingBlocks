58ec526048cf88e8fa0666bdc33bd785c2535b04
wzh425 <wuzihao425@outlook.com>
2022-07-14 13:33:25 +0800

    refactor：iMcClient add NotificationService

--- src/BasicAbility/Masa.BuildingBlocks.BasicAbility.Mc/IMcClient.cs
+++ src/BasicAbility/Masa.BuildingBlocks.BasicAbility.Mc/IMcClient.cs
@@ -14,4 +14,6 @@ public interface IMcClient
     IReceiverGroupService ReceiverGroupService { get; }
 
     IWebsiteMessageService WebsiteMessageService { get; }
+
+    INotificationService NotificationService { get; }
 }
