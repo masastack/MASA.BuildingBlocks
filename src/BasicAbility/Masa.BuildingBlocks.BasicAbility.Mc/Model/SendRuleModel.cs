046497dadce2d00043750c250fca5f7f6883d32f
wzh425 <wuzihao425@outlook.com>
2022-07-25 11:37:07 +0800

    refactor：sendrulemodel property change

--- src/BasicAbility/Masa.BuildingBlocks.BasicAbility.Mc/Model/SendRuleModel.cs
+++ src/BasicAbility/Masa.BuildingBlocks.BasicAbility.Mc/Model/SendRuleModel.cs
@@ -5,13 +5,7 @@ namespace Masa.BuildingBlocks.BasicAbility.Mc.Model;
 
 public class SendRuleModel
 {
-    public bool IsSendingInterval { get; set; }
-
-    public long SendingInterval { get; set; }
-
+    public bool IsCustom { get; set; }
+    public string CronExpression { get; set; } = string.Empty;
     public long SendingCount { get; set; }
-
-    public bool IsTiming { get; set; }
-
-    public DateTimeOffset? SendTime { get; set; }
 }
